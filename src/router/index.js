5c56b0221b100ae60cdad1d1a66727875b2ebe4d
suncheng <157600408@qq.com>
2019-02-12 21:01:51 +0800

    完整的路由解析流程“

--- src/router/index.js
+++ src/router/index.js
@@ -27,4 +27,19 @@ router.afterEach((to, from) => {
   // logining = false
 })
 
+/**
+ * 1. 导航被触发
+ * 2. 在失活的组件（即将离开的页面组件）里调用离开路透守卫 beforeRouteLeave
+ * 3. 调用全局的前置守卫 beforeEach
+ * 4. 在重用的组件里调用 beforeRouteUpdate
+ * 5. 调用路由独享守卫 beforeEnter
+ * 6. 解析异步路由组件
+ * 7. 在被激活的组件 （即将进入的页面组件） 内调用 beforeRouteEnter
+ * 8. 调用全局的解析守卫 beforeResolve
+ * 9. 导航被确认
+ * 10. 调用全局的后置守卫 afterEach
+ * 11. 触发 DOM 更新
+ * 12. 用创建好的实例调用 beforeRouteEnter 守卫里传给 next 的回调函数
+ */
+
 export default router
