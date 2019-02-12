2352ef05586a5c5e116a9286cd95370c22f96721
suncheng <157600408@qq.com>
2019-02-12 22:40:05 +0800

    bus 模式实现页面组件之间传值

--- src/views/email.vue
+++ src/views/email.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    email: 1563@163.com
+  <div class="email">
+    <button @click="handleClick">按我</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    handleClick () {
+      this.$bus.$emit('on-click', 'Hello World')
+      this.$emit('on-db', 'Hello Vue')
+    }
+  },
+  mounted () {
+    this.$on('on-db', mes => {
+      console.log(mes)
+    })
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.email {
+  border: 1px solid red;
+}
 </style>
--- src/views/tel.vue
+++ src/views/tel.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-    tel: 12345678901
+  <div class="tel">
+    {{ message }}
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      message: ''
+    }
+  },
+  mounted () {
+    this.$bus.$on('on-click', mes => {
+      this.message = mes
+    })
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.tel {
+  border: 1px solid green
+}
 </style>
