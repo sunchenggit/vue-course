f60cc375d61b8b9ca4b731017d5fbc2572637970
suncheng <157600408@qq.com>
2019-02-12 22:16:31 +0800

    父子组件之间的通信

---
+++ src/components/AInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input @input="handleInput" :value="value">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AInput',
+  props: {
+    value: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  methods: {
+    handleInput (event) {
+      const value = event.target.value
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/store.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- <a-input v-model="inputValue" /> -->
+    <a-input :value="inputValue" @input="handleInput" />
+    <p>{{ inputValue }}</p>
+  </div>
+</template>
+
+<script>
+import AInput from '_c/AInput.vue'
+export default {
+  name: 'store',
+  data () {
+    return {
+      inputValue: ''
+    }
+  },
+  components: {
+    AInput
+  },
+  methods: {
+    handleInput (val) {
+      this.inputValue = val
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
